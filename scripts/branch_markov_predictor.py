19382807be403a6196536741f346933661b6bcfe
PaulRosu <timprosu@gmail.com>
2025-01-03 02:20:15 +0200

    Include Core ID in Branch Handling

--- scripts/branch_markov_predictor.py
+++ scripts/branch_markov_predictor.py
@@ -17,17 +17,22 @@ Core state values:
 - NUM_STATES     7
 """
 
+import sim
+import os
+import csv
+
 class BranchMarkovPredictor:
     def setup(self, args):
         self.branch_count = 0
         sim.util.EveryBranch(self.handle_branch)
 
-    def handle_branch(self, ip, predicted, actual, indirect):
+    def handle_branch(self, ip, predicted, actual, indirect, core_id):
         self.branch_count += 1
         if self.branch_count % 1000 == 0:  # Print periodically
             print("[BRANCH_MARKOV] Branch #{0}".format(self.branch_count))
             print("  IP: {0}".format(hex(ip)))
             print("  Predicted: {0}, Actual: {1}".format(predicted, actual))
+            print("  Core: {0}".format(core_id))
             print("  {0}".format('Correct!' if predicted == actual else 'Incorrect'))
 
 sim.util.register(BranchMarkovPredictor())
